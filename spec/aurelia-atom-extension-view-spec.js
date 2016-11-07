4807045a9fac0dcaefc42ec9e50e9b98b7e85c24
Erik Lieben <erik.lieben@effectory.com>
2016-11-07 13:38:54 +0100

    init

---
+++ spec/aurelia-atom-extension-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AureliaAtomExtensionView from '../lib/aurelia-atom-extension-view';
+
+describe('AureliaAtomExtensionView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
